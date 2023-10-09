c798ed54825f06500d067e6b0d79327a15e01236
Kaan <kaansimsek.dev@gmail.com>
2023-10-09 21:47:41 +0300

    ui

--- app/src/main/java/com/mertyigit0/bodycheck/BMIFragment.kt
+++ app/src/main/java/com/mertyigit0/bodycheck/BMIFragment.kt
@@ -5,20 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mertyigit0.bodycheck.databinding.FragmentBMIBinding
 
 class BMIFragment : Fragment() {
+
+    private var _binding: FragmentBMIBinding? = null;
+    private val binding get() = _binding!!;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_b_m_i, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        _binding = FragmentBMIBinding.inflate(inflater,container,false)
+        val view = binding.root;
+        return view
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
